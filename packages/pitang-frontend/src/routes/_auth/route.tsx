4eee43ff1efd2cafb970cc1365a969f5bebdfc5c
Melissa Salinas <melissa.salinas.ruiz@gmail.com>
2026-03-22 18:55:29 -0300

    Center auth layout to full screen

--- packages/pitang-frontend/src/routes/_auth/route.tsx
+++ packages/pitang-frontend/src/routes/_auth/route.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { GalleryVerticalEnd } from "lucide-react";
 
 export const Route = createFileRoute("/_auth")({
   component: RouteComponent,
@@ -7,28 +6,9 @@ export const Route = createFileRoute("/_auth")({
 
 function RouteComponent() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <Outlet />
-          </div>
-        </div>
-      </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="https://ui.shadcn.com/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-sm">
+        <Outlet />
       </div>
     </div>
   );
