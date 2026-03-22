b0684e21086e167955e05eab90a234b8c3c8cf7d
Melissa Salinas <melissa.salinas.ruiz@gmail.com>
2026-03-21 21:50:07 -0300

    Prevents logged in user from accessing register page and redirects to dashboard

--- packages/pitang-frontend/src/routes/_auth/register.tsx
+++ packages/pitang-frontend/src/routes/_auth/register.tsx
@@ -1,8 +1,19 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { SignupForm } from "@/components/signup-form";
 
 export const Route = createFileRoute("/_auth/register")({
+   beforeLoad: () => {
+      const token = document.cookie
+        .split("; ")
+        .find((c) => c.startsWith("@pitang/accessToken="))
+        ?.split("=")[1];
+  
+        if (token) {
+          throw redirect ({to: "/dashboard"}); 
+        }
+    },
+
   component: RouteComponent,
 });
 
