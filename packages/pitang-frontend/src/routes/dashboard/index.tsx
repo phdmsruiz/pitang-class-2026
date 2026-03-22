d43d18ab4b228041c504b8b71c643534babdd156
Melissa Salinas <melissa.salinas.ruiz@gmail.com>
2026-03-21 21:51:13 -0300

    Adds logout button to dashboard

--- packages/pitang-frontend/src/routes/dashboard/index.tsx
+++ packages/pitang-frontend/src/routes/dashboard/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useAuth } from "@/hooks/use-auth";
 
 export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/"!</div>;
+  const { handleLogout } = useAuth();
+
+  return (
+    <div>
+      <h1>Hello "/dashboard/"!</h1>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
